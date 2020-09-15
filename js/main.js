0630256e8a49fa83aa15db3b7f6c2669b33f6036
ArmorDRX97 <armor.d.r.x@gmail.com>
2020-09-15 15:33:48 +0600

    s

--- js/main.js
+++ js/main.js
@@ -37,10 +37,21 @@ $( document ).ready(function() {
         $droparea.addClass('is-active');
     });
 
-// back to normal state
     $fileInput.on('dragleave blur drop', function() {
         $droparea.removeClass('is-active');
     });
 
+    $fileInput.on('change', function() {
+        var filesCount = $(this)[0].files.length;
+        var $textContainer = $(this).prev();
 
+        if (filesCount === 1) {
+            // if single file is selected, show file name
+            var fileName = $(this).val().split('\\').pop();
+            $textContainer.text(fileName);
+        } else {
+            // otherwise show number of files
+            $textContainer.text(filesCount + ' files selected');
+        }
+    });
 });

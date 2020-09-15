2e6b8313746c9fbc6ca88e728328d65b6490881e
ArmorDRX97 <armor.d.r.x@gmail.com>
2020-09-15 15:18:45 +0600

    s

--- js/main.js
+++ js/main.js
@@ -1,16 +1,46 @@
 $( document ).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+
     $("#colorpicker").spectrum({
         color: "#f00"
+    });
+
+    $("#colorpicker-btn").spectrum({
+        color: "#333",
+        move: function(color) {
+            $(this).css("background-color", color.toHexString());
+            $('.shop .btn').css("background-color", color.toHexString());
+        },
+    });
+
+    $("#colorpicker-btn-text").spectrum({
+        color: "#fff",
+        move: function(color) {
+            $(this).css("background-color", color.toHexString());
+            $('.shop .btn').css("color", color.toHexString());
+        },
     });
 
     $('.global-btn').click(function () {
         $('.settings-panel').addClass('show-global-settings')
     })
 
-    $('body').click(function (event)
-    {
-        if(!$(event.target).closest('.settings-panel').length && !$(event.target).is('.settings-panel')) {
-            $(".settings-panel").removeClass('show-global-settings')
-        }
+
+    $('.global-settings .btn-close').click(function () {
+        $('.settings-panel').removeClass('show-global-settings')
+    })
+
+    let $fileInput = $('.file-input');
+    let $droparea = $('.file-drop-area');
+
+    $fileInput.on('dragenter focus click', function() {
+        $droparea.addClass('is-active');
     });
+
+// back to normal state
+    $fileInput.on('dragleave blur drop', function() {
+        $droparea.removeClass('is-active');
+    });
+
+
 });
